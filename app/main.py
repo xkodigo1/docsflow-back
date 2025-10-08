5defcafd3afa48ed989bde84e63e61a0ac7e7c87
xkodigo1 <180666585+xkodigo1@users.noreply.github.com>
2025-10-08 13:42:24 -0500

    Add user_stats router to FastAPI application

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from controllers.documents import router as documents_router
 from controllers.tables import router as tables_router
 from controllers.users import router as users_router
 from controllers.departments import router as departments_router
+from controllers.user_stats import router as user_stats_router
 from fastapi.middleware.cors import CORSMiddleware
 
 application = FastAPI()
@@ -38,3 +39,4 @@ application.include_router(tables_router)
 application.include_router(users_router)
 application.include_router(documents_router)
 application.include_router(departments_router)
+application.include_router(user_stats_router)
